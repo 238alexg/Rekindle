6055674034c167b41cb8eb8ef8e47aef96ed6a10
PalmerHogen <palmerhogen@gmail.com>
2019-01-26 14:46:05 -0800

    pushing static class to represent the world

---
+++ Assets/Scripts/World/World.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public static class World
+{
+    public static int Width, Height;
+    public static object[,] WorldTiles = new object[Width, Height];
+
+    public static void PlaceObjectAtLocation(object objectToPlace, int x, int y)
+    {
+        WorldTiles[x, y] = objectToPlace;
+    }
+
+    public static void PlaceObjectAtLocation(object objectToPlace, Vector2Int location)
+    {
+        PlaceObjectAtLocation(objectToPlace, location.x, location.y);
+    }
+
+    public static void PlaceObjectAtLocation(object objectToPlace, Vector2 location)
+    {
+        var intLocation = Vector2Int.FloorToInt(location);
+        PlaceObjectAtLocation(objectToPlace, intLocation);
+    }
+
+    public static object GetObjectAtLocation(int x, int y)
+    {
+        return WorldTiles[x, y];
+    }
+
+    public static object GetObjectAtLocation(Vector2Int location)
+    {
+        return GetObjectAtLocation(location.x, location.y);
+    }
+
+    public static object GetObjectAtLocation(Vector2 location)
+    {
+        var intLocation = Vector2Int.FloorToInt(location);
+        return GetObjectAtLocation(intLocation);
+    }
+}
