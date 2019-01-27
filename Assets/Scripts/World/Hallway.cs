d5f38cac068bfd50dcb50ab23bb079f8ca5b3b05
dan-su <dsu@uoregon.edu>
2019-01-26 19:23:59 -0800

    add entryWay class to Hallway Class

--- Assets/Scripts/World/Hallway.cs
+++ Assets/Scripts/World/Hallway.cs
@@ -3,4 +3,12 @@
 public class Hallway : Space
 {
     public HashSet<Room> Rooms;
+    public Room StartRoom;
+	public int Length;
+
+    public void entryWay(Room startRoom, int length)
+    {
+        StartRoom = startRoom;
+        Length = length;
+    }
 }
