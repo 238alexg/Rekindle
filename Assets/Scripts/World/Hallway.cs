39cb256d869050e4524bfe1f12cb615b9b8a92c5
PalmerHogen <palmerhogen@gmail.com>
2019-01-26 20:05:29 -0800

    Modifying Map Representation

--- Assets/Scripts/World/Hallway.cs
+++ Assets/Scripts/World/Hallway.cs
@@ -1,14 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class Hallway : Space
 {
     public HashSet<Room> Rooms;
-    public Room StartRoom;
 	public int Length;
-
-    public void entryWay(Room startRoom, int length)
-    {
-        StartRoom = startRoom;
-        Length = length;
-    }
 }
--- Assets/Scripts/World/Space.cs
+++ Assets/Scripts/World/Space.cs
@@ -3,5 +3,6 @@ using System.Collections.Generic;
 
 public abstract class Space : MonoBehaviour
 {
+    public Vector2 Position;
     public HashSet<Entity> Entities;
 }
