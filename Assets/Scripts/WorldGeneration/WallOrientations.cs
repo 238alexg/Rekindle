78a920b7192aa7fdd30058db5f7fe26762861343
aidanPans <aidan.potts@icloud.com>
2019-01-27 15:30:52 -0800

    WallOrientations.cs changes for inner walls and corners

--- Assets/Scripts/WorldGeneration/WallOrientations.cs
+++ Assets/Scripts/WorldGeneration/WallOrientations.cs
@@ -13,7 +13,15 @@ public class WallOrientations : ScriptableObject
 		East,
 		South,
 		Southwest,
-		Southeast
+		Southeast,
+        InnerNorthWest,
+        InnerNorthEast,
+        InnerSouthEast,
+        InnerSouthWest,
+        UpperLeftCorner,
+        UpperRightCorner,
+        LowerRightCorner,
+        LowerLeftCorner
 	}
 
 	public TileBase NorthFacingWall;
@@ -25,7 +33,17 @@ public class WallOrientations : ScriptableObject
 	public TileBase SouthwestFacingWall;
 	public TileBase SoutheastFacingWall;
 
-	public TileBase GetWall(WallDirection direction)
+    public TileBase InnerNorthWestFacingWall;
+    public TileBase InnerNorthEastFacingWall;
+    public TileBase InnerSouthEastFacingWall;
+    public TileBase InnerSouthWestFacingWall;
+
+    public TileBase UpperLeftCorner;
+    public TileBase UpperRightCorner;
+    public TileBase LowerRightCorner;
+    public TileBase LowerLeftCorner;
+
+    public TileBase GetWall(WallDirection direction)
 	{
 		switch (direction)
 		{
@@ -45,7 +63,23 @@ public class WallOrientations : ScriptableObject
 				return SouthwestFacingWall;
 			case WallDirection.Southeast:
 				return SoutheastFacingWall;
-			default:
+            case WallDirection.InnerNorthWest:
+                return InnerNorthWestFacingWall;
+            case WallDirection.InnerNorthEast:
+                return InnerNorthEastFacingWall;
+            case WallDirection.InnerSouthEast:
+                return InnerSouthEastFacingWall;
+            case WallDirection.InnerSouthWest:
+                return InnerSouthWestFacingWall;
+            case WallDirection.UpperLeftCorner:
+                return UpperLeftCorner;
+            case WallDirection.UpperRightCorner:
+                return UpperRightCorner;
+            case WallDirection.LowerRightCorner:
+                return LowerRightCorner;
+            case WallDirection.LowerLeftCorner:
+                return LowerLeftCorner;
+            default:
 				return null;
 		}
 	}
