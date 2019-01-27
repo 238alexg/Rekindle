6bd254df3c58d26a366f036d761dd6ce7482ad15
PalmerHogen <palmerhogen@gmail.com>
2019-01-27 10:59:35 -0800

    Re-commiting audio Scripts

---
+++ Assets/Scripts/Audio/AudioClipCache.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AudioClipCache : ScriptableObject
+{
+    public AudioClip DrumLoop;
+    public AudioClip ShakerLoop;
+    public AudioClip HappyBassLoop;
+    public AudioClip SadBassLoop;
+    public AudioClip HappyPianoLoop;
+    public AudioClip SadPianoLoop;
+    public AudioClip HappyHornLoop;
+    public AudioClip SadHornLoop;
+    public AudioClip HappyMainMelodyLoop;
+    public AudioClip SadMainMelodyLoop;
+    public AudioClip HappyCounterMelodyLoop;
+    public AudioClip SadCounterMelodyLoop;
+    public AudioClip HappyGlockLoop;
+    public AudioClip SadGlockLoop;
+}
