7d0dba8062b515abdb0b2a5c14c57a6fa3386d3b
PalmerHogen <palmerhogen@gmail.com>
2019-01-27 13:35:36 -0800

    Modifying AudioManager to make PlayEffect callable from editor

--- Assets/Scripts/Audio/MusicPlayer.cs
+++ Assets/Scripts/Audio/MusicPlayer.cs
@@ -6,6 +6,8 @@ public class MusicPlayer
 {
     static readonly float SecondsPerMeasure = 1f / 3f;
 
+    WaitForSeconds WaitForMeasure = new WaitForSeconds(SecondsPerMeasure);
+
     static MusicPlayer _Inst;
     static GameObject AudioTransportManager;
     public static MusicPlayer Inst
@@ -28,12 +30,12 @@ public class MusicPlayer
 
     AudioSource Drums;
     AudioSource Shakers;
-    AudioSource Bass;
-    AudioSource Piano;
-    AudioSource Horns;
-    AudioSource MainMelody;
-    AudioSource CounterMelody;
-    AudioSource Glock;
+    AudioSource BassH, BassS;
+    AudioSource PianoH, PianoS;
+    AudioSource HornsH, HornsS;
+    AudioSource MainMelodyH, MainMelodyS;
+    AudioSource CounterMelodyH, CounterMelodyS;
+    AudioSource GlockH, GlockS;
 
     AudioClip DrumLoop;
     AudioClip ShakerLoop;
@@ -54,30 +56,58 @@ public class MusicPlayer
     {
         Drums = AudioTransportManager.AddComponent<AudioSource>();
         Shakers = AudioTransportManager.AddComponent<AudioSource>();
-        Bass = AudioTransportManager.AddComponent<AudioSource>();
-        Piano = AudioTransportManager.AddComponent<AudioSource>();
-        Horns = AudioTransportManager.AddComponent<AudioSource>();
-        MainMelody = AudioTransportManager.AddComponent<AudioSource>();
-        CounterMelody = AudioTransportManager.AddComponent<AudioSource>();
-        Glock = AudioTransportManager.AddComponent<AudioSource>();
+
+        BassH = AudioTransportManager.AddComponent<AudioSource>();
+        PianoH = AudioTransportManager.AddComponent<AudioSource>();
+        HornsH = AudioTransportManager.AddComponent<AudioSource>();
+        MainMelodyH = AudioTransportManager.AddComponent<AudioSource>();
+        CounterMelodyH = AudioTransportManager.AddComponent<AudioSource>();
+        GlockH = AudioTransportManager.AddComponent<AudioSource>();
+
+        BassS = AudioTransportManager.AddComponent<AudioSource>();
+        PianoS = AudioTransportManager.AddComponent<AudioSource>();
+        HornsS = AudioTransportManager.AddComponent<AudioSource>();
+        MainMelodyS = AudioTransportManager.AddComponent<AudioSource>();
+        CounterMelodyS = AudioTransportManager.AddComponent<AudioSource>();
+        GlockS = AudioTransportManager.AddComponent<AudioSource>();
     }
 
     public void InitializeClips(AudioClipCache audioClips)
     {
         Drums.clip = DrumLoop = audioClips.DrumLoop;
         Shakers.clip = ShakerLoop = audioClips.ShakerLoop;
-        HappyBassLoop = audioClips.HappyBassLoop;
-        Bass.clip = SadBassLoop = audioClips.SadBassLoop;
-        HappyPianoLoop = audioClips.HappyPianoLoop;
-        Piano.clip = SadPianoLoop = audioClips.SadPianoLoop;
-        HappyHornLoop = audioClips.HappyHornLoop;
-        Horns.clip = SadHornLoop = audioClips.SadHornLoop;
-        HappyMainMelodyLoop = audioClips.HappyMainMelodyLoop;
-        MainMelody.clip = SadMainMelodyLoop = audioClips.SadMainMelodyLoop;
-        HappyCounterMelodyLoop = audioClips.HappyCounterMelodyLoop;
-        CounterMelody.clip = SadCounterMelodyLoop = audioClips.SadCounterMelodyLoop;
-        HappyGlockLoop = audioClips.HappyGlockLoop;
-        Glock.clip = SadGlockLoop = audioClips.SadGlockLoop;
+        BassH.clip = HappyBassLoop = audioClips.HappyBassLoop;
+        BassS.clip = SadBassLoop = audioClips.SadBassLoop;
+        PianoH.clip = HappyPianoLoop = audioClips.HappyPianoLoop;
+        PianoS.clip = SadPianoLoop = audioClips.SadPianoLoop;
+        HornsH.clip = HappyHornLoop = audioClips.HappyHornLoop;
+        HornsS.clip = SadHornLoop = audioClips.SadHornLoop;
+        MainMelodyH.clip = audioClips.HappyMainMelodyLoop;
+        MainMelodyS.clip = SadMainMelodyLoop = audioClips.SadMainMelodyLoop;
+        CounterMelodyH.clip = HappyCounterMelodyLoop = audioClips.HappyCounterMelodyLoop;
+        CounterMelodyS.clip = SadCounterMelodyLoop = audioClips.SadCounterMelodyLoop;
+        GlockH.clip = HappyGlockLoop = audioClips.HappyGlockLoop;
+        GlockS.clip = SadGlockLoop = audioClips.SadGlockLoop;
+
+        BassS.clip = SadBassLoop;
+        PianoS.clip = SadPianoLoop;
+        HornsS.clip = SadHornLoop;
+        MainMelodyS.clip = SadMainMelodyLoop;
+        CounterMelodyS.clip = SadCounterMelodyLoop;
+        GlockS.clip = SadGlockLoop;
+
+        BassH.clip = HappyBassLoop;
+        BassH.volume = 0f;
+        PianoH.clip = HappyPianoLoop;
+        PianoH.volume = 0f;
+        HornsH.clip = HappyHornLoop;
+        HornsH.volume = 0f;
+        MainMelodyH.clip = HappyMainMelodyLoop;
+        MainMelodyH.volume = 0f;
+        CounterMelodyH.clip = HappyCounterMelodyLoop;
+        CounterMelodyH.volume = 0f;
+        GlockH.clip = HappyGlockLoop;
+        GlockH.volume = 0f;
 
         IsLooping = true;
         Play();
@@ -106,12 +136,18 @@ public class MusicPlayer
             return
             Drums.isPlaying ||
             Shakers.isPlaying ||
-            Bass.isPlaying ||
-            Piano.isPlaying ||
-            Horns.isPlaying ||
-            MainMelody.isPlaying ||
-            CounterMelody.isPlaying ||
-            Glock.isPlaying;
+            BassS.isPlaying ||
+            BassH.isPlaying ||
+            PianoS.isPlaying ||
+            PianoH.isPlaying ||
+            HornsS.isPlaying ||
+            HornsH.isPlaying ||
+            MainMelodyS.isPlaying ||
+            MainMelodyH.isPlaying ||
+            CounterMelodyS.isPlaying ||
+            CounterMelodyH.isPlaying ||
+            GlockS.isPlaying ||
+            GlockH.isPlaying;
         }
     }
 
@@ -122,23 +158,35 @@ public class MusicPlayer
             return
             Drums.loop ||
             Shakers.loop ||
-            Bass.loop ||
-            Piano.loop ||
-            Horns.loop ||
-            MainMelody.loop ||
-            CounterMelody.loop ||
-            Glock.loop;
+            BassS.loop ||
+            BassH.loop ||
+            PianoS.loop ||
+            PianoH.loop ||
+            HornsS.loop ||
+            HornsH.loop ||
+            MainMelodyS.loop ||
+            MainMelodyH.loop ||
+            CounterMelodyS.loop ||
+            CounterMelodyH.loop ||
+            GlockS.loop ||
+            GlockH.loop;
         }
         set
         {
             Drums.loop =
             Shakers.loop =
-            Bass.loop =
-            Piano.loop =
-            Horns.loop =
-            MainMelody.loop =
-            CounterMelody.loop =
-            Glock.loop = value;
+            BassS.loop =
+            BassH.loop =
+            PianoS.loop =
+            PianoH.loop =
+            HornsS.loop =
+            HornsH.loop =
+            MainMelodyS.loop =
+            MainMelodyH.loop =
+            CounterMelodyS.loop =
+            CounterMelodyH.loop =
+            GlockS.loop =
+            GlockH.loop = value;
         }
     }
 
@@ -152,38 +200,56 @@ public class MusicPlayer
         {
             Drums.time =
             Shakers.time =
-            Bass.time =
-            Piano.time =
-            Horns.time =
-            MainMelody.time =
-            CounterMelody.time =
-            Glock.time = value;
+            BassS.time =
+            BassH.time =
+            PianoS.time =
+            PianoH.time =
+            HornsS.time =
+            HornsH.time =
+            MainMelodyS.time =
+            MainMelodyH.time =
+            CounterMelodyS.time =
+            CounterMelodyH.time =
+            GlockS.time =
+            GlockH.time = value;
         }
+    }
+
+    void SwapChordTrack(AudioSource chordPlayer1, AudioSource chordPlayer2)
+    {
+        var volume1 = chordPlayer1.volume;
+        var volume2 = chordPlayer2.volume;
+
+        AudioManager.Inst.StartCoroutine(FadeTo(volume1 > 0 ? 0 : volume2, chordPlayer1));
+        AudioManager.Inst.StartCoroutine(FadeTo(volume2 > 0 ? 0 : volume1, chordPlayer2));
+    }
+
+    bool VolumeIsFading = false;
+
+    IEnumerator FadeTo(float amplitude, AudioSource sourceToFade)
+    {
+        var startVolume = sourceToFade.volume;
+        float lerpAmt = 0f;
+        VolumeIsFading = true;
+        while (!MeasureReached)
+        {
+            sourceToFade.volume = Mathf.Lerp(startVolume, amplitude, lerpAmt += Time.deltaTime);
+            yield return null;
+        }
+        VolumeIsFading = false;
     }
 
     void SwapChords()
     {
         float loopTime = SampleTime;
-        if (UsingHappyChords)
-        {
-            Bass.clip = SadBassLoop;
-            Piano.clip = SadPianoLoop;
-            Horns.clip = SadHornLoop;
-            MainMelody.clip = SadMainMelodyLoop;
-            CounterMelody.clip = SadCounterMelodyLoop;
-            Glock.clip = SadGlockLoop;
-            UsingHappyChords = false;
-        }
-        else
-        {
-            Bass.clip = HappyBassLoop;
-            Piano.clip = HappyPianoLoop;
-            Horns.clip = HappyHornLoop;
-            MainMelody.clip = HappyMainMelodyLoop;
-            CounterMelody.clip = HappyCounterMelodyLoop;
-            Glock.clip = HappyGlockLoop;
-            UsingHappyChords = true;
-        }
+
+        SwapChordTrack(BassS, BassH);
+        SwapChordTrack(PianoS, PianoH);
+        SwapChordTrack(HornsS, HornsH);
+        SwapChordTrack(MainMelodyS, MainMelodyH);
+        SwapChordTrack(CounterMelodyS, CounterMelodyH);
+        SwapChordTrack(GlockS, GlockH);
+
         SampleTime = loopTime;
         Play();
 
@@ -199,15 +265,27 @@ public class MusicPlayer
 
     public void UpdateMood(float mood)
     {
-        // 0f == despondent
-        // 1f == good times
-        Piano.volume = Mathf.Lerp(1f, 0.25f, mood);
-        Drums.volume = mood > 0.2f ? Mathf.Lerp(0f, .75f, mood) : 0f;
-        Glock.volume = Bass.volume = Drums.volume;
-        Horns.volume = mood > 0.5f ? 0.75f : Mathf.Lerp(0f, 0.75f, 2 * mood);
-        MainMelody.volume = Mathf.Lerp(0f, 1f, mood);
-        CounterMelody.volume = mood < 0.5f ? Mathf.Lerp(0f, .5f, 2 * mood) : Mathf.Lerp(1f, 0f, mood);
-        Shakers.volume = Mathf.Lerp(0f, .75f, mood);
+        if (VolumeIsFading)
+            return;
+
+        Drums.volume = mood > 0.2f ? Mathf.Lerp(0f, .75f, mood * mood) : 0f;
+        Shakers.volume = mood > 0 ? Mathf.Lerp(0f, .5f, mood * mood) : 0f;
+        if (UsingHappyChords)
+        {
+            PianoH.volume = Mathf.Lerp(1f, 0.25f, mood);
+            GlockH.volume = BassH.volume = Drums.volume;
+            HornsH.volume = mood > 0.5f ? 0.75f : Mathf.Lerp(0f, 0.75f, 2 * mood);
+            MainMelodyH.volume = Mathf.Lerp(.25f, 1f, mood);
+            CounterMelodyH.volume = mood < 0.5f ? Mathf.Lerp(0f, .5f, 2 * mood) : Mathf.Lerp(1f, 0f, mood);
+        }
+        else
+        {
+            PianoS.volume = Mathf.Lerp(1f, 0.25f, mood);
+            GlockS.volume = BassS.volume = Drums.volume;
+            HornsS.volume = mood > 0.5f ? 0.75f : Mathf.Lerp(0f, 0.75f, 2 * mood);
+            MainMelodyS.volume = Mathf.Lerp(.25f, 1f, mood);
+            CounterMelodyS.volume = mood < 0.5f ? Mathf.Lerp(0f, .5f, 2 * mood) : Mathf.Lerp(1f, 0f, mood);
+        }
 
         if (CurrentMood < 0.75f && mood > 0.75f || CurrentMood > 0.75f && mood < 0.75f)
             Application.Inst.StartCoroutine(SwapChordsAtNextMeasure());
@@ -219,24 +297,36 @@ public class MusicPlayer
     {
         Drums.Play();
         Shakers.Play();
-        Bass.Play();
-        Piano.Play();
-        Horns.Play();
-        MainMelody.Play();
-        CounterMelody.Play();
-        Glock.Play();
+        BassS.Play();
+        BassH.Play();
+        PianoS.Play();
+        PianoH.Play();
+        HornsS.Play();
+        HornsH.Play();
+        MainMelodyS.Play();
+        MainMelodyH.Play();
+        CounterMelodyS.Play();
+        CounterMelodyH.Play();
+        GlockS.Play();
+        GlockH.Play();
     }
 
     public void Pause()
     {
         Drums.Pause();
         Shakers.Pause();
-        Bass.Pause();
-        Piano.Pause();
-        Horns.Pause();
-        MainMelody.Pause();
-        CounterMelody.Pause();
-        Glock.Pause();
+        BassS.Pause();
+        BassH.Pause();
+        PianoS.Pause();
+        PianoH.Pause();
+        HornsS.Pause();
+        HornsH.Pause();
+        MainMelodyS.Pause();
+        MainMelodyH.Pause();
+        CounterMelodyS.Pause();
+        CounterMelodyH.Pause();
+        GlockS.Pause();
+        GlockH.Pause();
     }
 
 }
