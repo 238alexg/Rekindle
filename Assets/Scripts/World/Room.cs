871b59dfd900b51a16a657f995ae64c3bfbb689d
PalmerHogen <palmerhogen@gmail.com>
2019-01-26 16:42:16 -0800

    re-architecture of world

---
+++ Assets/Scripts/World/Room.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+public class Room : Space
+{
+    public HashSet<Hallway> Hallways;
+    public int Width, Height;
+}
---
+++ Assets/Scripts/World/Space.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+public abstract class Space
+{
+    public HashSet<Entity> Entities;
+}
