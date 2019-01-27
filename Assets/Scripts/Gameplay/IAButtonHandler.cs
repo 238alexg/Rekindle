4dcd3ca4be36d7364ea5328f62f0b24b5b74d6dc
Alex Geoffrey <apg@uoregon.edu>
2019-01-26 21:52:13 -0800

    Added button handlers for interactable tiles, and created an obstacle class

---
+++ Assets/Scripts/Gameplay/IAButtonHandler.cs
@@ -0,0 +1,4 @@
+﻿public interface IAButtonHandler
+{
+	void OnAButtonPress(Player player);
+}
---
+++ Assets/Scripts/World/Obstacle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Obstacle : TileBase, IAButtonHandler, IBButtonHandler
+{
+	public event Action<bool> ObstacleLockChangedEvent = delegate { };
+
+	public Item Solution;
+	public Vector3Int Position;
+
+	bool Unlocked = false;
+	
+	void TryUnlockObstacle(Player player)
+	{
+		foreach (var item in player.Inventory)
+		{
+			if (item == Solution)
+			{
+				UnlockObstacle();
+			}
+		}
+	}
+
+	public void RelockObstacle()
+	{
+		ObstacleLockChangedEvent(false);
+	}
+
+	void UnlockObstacle()
+	{
+		Unlocked = true;
+		ObstacleLockChangedEvent(true);
+	}
+
+	public void OnAButtonPress(Player player)
+	{
+		TryUnlockObstacle(player);
+	}
+
+	public void OnBButtonPress(Player player)
+	{
+		RelockObstacle();
+	}
+}
