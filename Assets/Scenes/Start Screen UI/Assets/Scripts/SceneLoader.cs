6b39868c06ed418321c9bd6263cc9772a84bcc74
Garrett Brown <gbrown9816@gmail.com>
2019-01-26 22:38:39 -0800

    adding opening/closing sound effects and created a work in progress for the basic UI

---
+++ Assets/Scenes/Start Screen UI/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+    public AudioSource AudioPlayer;
+    
+    public void LoadScene(int level)
+    {
+        while(AudioPlayer.isPlaying)
+        {
+            ;
+        }
+        Application.LoadLevel(level);
+    }
+ 
+}
